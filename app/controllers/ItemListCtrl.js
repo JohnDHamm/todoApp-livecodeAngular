673619797c83019b5001a8d431d80aa399db8a0f
John Hamm <johndhamm1@gmail.com>
2016-07-21 12:22:55 -0500

    modularized with routing, partials and factory

---
+++ app/controllers/ItemListCtrl.js
@@ -0,0 +1,7 @@
+"use strict";
+
+app.controller("ItemListCtrl", function($scope, ItemStorage) {
+
+	$scope.items = ItemStorage.getItemList();	
+
+});
---
+++ app/controllers/ItemViewCtrl.js
@@ -0,0 +1,5 @@
+"use strict";
+
+app.controller("ItemViewCtrl", function($scope) {
+	$scope.message = "this is an item(placeholder)";
+});
